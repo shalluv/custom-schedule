0a6139af182684be8b9542843b3788b70d8f2a32
shalluv <leon555thai@gmail.com>
2024-03-07 16:15:54 +0700

    refactor: extract calculate column count function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function calculateColCount(start: string, end: string, step: number) {
+  const [startHour, startMinute] = start.split(':').map(Number);
+  const [endHour, endMinute] = end.split(':').map(Number);
+  const startTotalMinutes = startHour * 60 + startMinute;
+  const endTotalMinutes = endHour * 60 + endMinute;
+  const totalMinutes = endTotalMinutes - startTotalMinutes;
+  return totalMinutes / step;
+}
