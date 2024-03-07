0996f333d93193157ad8bc2798f49dd63989672f
shalluv <leon555thai@gmail.com>
2024-03-07 16:14:16 +0700

    feat: add padding to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Settings from '@/components/Settings';
 
 export default function Home() {
   return (
-    <main className="mx-auto flex w-full max-w-7xl flex-col gap-8 py-16">
+    <main className="mx-auto flex w-full max-w-7xl flex-col gap-8 p-4 py-16">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-3xl font-semibold">Schedule Customize</h1>
         <div className="flex items-center justify-between gap-4">
